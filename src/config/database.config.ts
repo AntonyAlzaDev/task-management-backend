e91b119bc38c752ff224b49038568e9a88ed8dca
AntonAlzaP <antony_alza@usmp.pe>
2025-09-21 02:02:48 -0500

    Add @nestjs/cli to devDependencies

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'dpg-d37pue8gjchc73cfo70g-a',
   port: parseInt(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres123',
-  database: process.env.DB_DATABASE || 'taskdb',
+  username: process.env.DB_USERNAME || 'taskuser',
+  password: process.env.DB_PASSWORD || 'm4jh8LJl3SLRDAqZEJ0GfRZggSiAF38H',
+  database: process.env.DB_DATABASE || 'taskdb_zkuy',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: false,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
